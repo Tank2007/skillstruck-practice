5660a38537c70cfd3e5fd8b16e13e4ed3ea2667e
Tank2007 <ccumm511@evit.edu>
2024-08-16 11:47:15 -0700

    exported from Skill Struck

---
+++ Input/Variable Types.py
@@ -0,0 +1,21 @@
+#String
+name = "Jon"
+print(name)
+#Integer
+int = 34
+print(int)
+#Boolean
+book = True
+book2 = False
+print(book)
+print(book2)
+#Challenge 1
+bmo = True
+bmo = False 
+#Challenge 2 
+name = True
+hi = "Hello!"
+int = 42 
+print(name)
+print(hi)
+print(int)
