74cd5fa72b41c2ed7156f0e7c5e61617e6800ae1
Tank2007 <ccumm511@evit.edu>
2024-08-23 11:43:48 -0700

    exported from Skill Struck

--- Input/Converting.py
+++ Input/Converting.py
@@ -1,12 +1,21 @@
 #Variables 
-age = "23"
-age = 34
-price = 24.0
+#age = "23"
+#age = 34
+#price = 24.0
 #print/variables
-banana = 2.8
-banana = int(2.8)
-print(banana)
-apple = 10
-apple = float(10)
-print(apple)
-#
+#banana = 2.8
+#banana = int(2.8)
+#print(banana)
+#apple = 10
+#apple = float(10)
+#print(apple)
+#Challenge 1 
+user_input = input("Enter a number with a decimal: ")
+float_value = float(user_input)
+int_value = int(float_value)
+print(int_value)
+#Challenge 2
+#age = input('Your age: ')
+#age = int(age)
+#age_in_18_years = age + 18
+#print(age_in_18_years)
