bf136c3d964efd0e6daf7e708c1654531c09dc2f
Tank2007 <ccumm511@evit.edu>
2024-08-23 11:43:48 -0700

    exported from Skill Struck

---
+++ Input/Concatenation.py
@@ -0,0 +1,26 @@
+# pizza = " pizza is good"
+# pie = "is a good dessert"
+# bread = " Samwich"
+# idk = pizza + " " + pie + bread
+# print(idk)
+
+#var1 = "still enrolling students"
+#var2 = "interested in getting help"
+#var3 = "with college admissio"
+
+#Challenge 1
+#sentence = var1 + " " + var2 + " " + var3
+#print(sentence)
+'''name = input ('what is your name? ')
+age = input('How old are you? ') 
+birthday = input('When is your birthday? ')
+#senctence 
+sentence2 = (f"Hi {name}! You are {age} years old and you were born on {birthday}.")
+print(sentence2)'''
+#Challenge 2 
+snickers = input("How many snickers you got? ")
+nerds = input("How many nerds you got? ")
+butterfingers = input("How many butterfingers you got? " )
+total = int(snickers) + int(nerds) + int(butterfingers) 
+senctence3 = (f"This year, you got {snickers} snickers, {nerds} nerds, and {butterfingers} butterfingers. The total number of these candies is {total} candies.")
+print (senctence3)
