a2cf7eb126ad81e7d49e711c0802ebc558178e9f
Tank2007 <ccumm511@evit.edu>
2024-08-29 12:12:56 -0700

    exported from Skill Struck

---
+++ Input/Strings.py
@@ -0,0 +1,14 @@
+line = '''Meet industry professionals working in Computer Science!  
+Learn about career opportunities in CS! This is a free program. 
+Sponsored by Industry volunteers & Education Empowers In.'''
+
+'''print(line)'''
+short_quote = " I can do all things through Christ which strengtheneth me."#Philippians 4:13
+long_quote = """When you stand before God, you cannot say,
+ But I was told by others to do thus. Or that, Virtue was not convenient at the time. This will not suffice.""" #King bladwin iv
+
+'''print(short_quote)
+print(long_quote)'''
+journal = """Went to school leard some history learnd some math and went to english.
+ read the bible in he moring woke up got on you tube wached some benny."""
+print(journal)  
