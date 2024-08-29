eae17394ce3c54bc797a55e08f15009a5ce9bc1a
Tank2007 <ccumm511@evit.edu>
2024-08-29 12:12:57 -0700

    exported from Skill Struck

---
+++ Input/String Methods.py
@@ -0,0 +1,14 @@
+'''string1 = "I like games"
+print(string1.lower())
+print(string1.upper())
+string2 = "I like games"
+print(string2.replace("cool", "fun"))'''
+#Challenges
+'''string1 = input("Enter a string that includes at least one '1': ")
+
+string2 = string1.replace("1", "one")
+print(string2)'''
+
+#Challenge 2
+string1 = input("Enter a message. ")
+print(string1.upper()) 
