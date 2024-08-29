c80b0fd27ba79b89e0d284d758c7e079800f04ae
Tank2007 <ccumm511@evit.edu>
2024-08-29 12:12:56 -0700

    exported from Skill Struck

--- Input/Modulus.py
+++ Input/Modulus.py
@@ -1,2 +1,18 @@
-'''apple = 10 % 3
+#prints 1 
+''''apple = 10 % 3
 print(apple)'''
+#prints 3
+'''hat = int(10/3)
+print(hat)
+nummber = 245
+print(nummber % 1)'''
+'''number = int(input())
+remainder = number % 7
+print(remainder)'''
+#challenge
+'''family_members = int(input())
+oranges = int(input())
+oranges_left = oranges % family_members
+print(oranges_left)'''
+number = int(input())
+print(number %100)
