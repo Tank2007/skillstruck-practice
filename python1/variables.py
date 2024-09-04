43846cd9359019b28649d3e49ed6ed84132e1678
Tank2007 <ccumm511@evit.edu>
2024-09-04 12:36:06 -0700

    exported from Skill Struck

---
+++ python1/variables.py
@@ -0,0 +1,7 @@
+what_goes_up_when_rain_comes_down = "umbrella"
+what_is_full_of_holes_but_holds_water = "sponge"
+what_can_you_not_keep_until_you_give_it = "your word"
+#print
+print(what_goes_up_when_rain_comes_down)
+print(what_is_full_of_holes_but_holds_water)
+print(what_can_you_not_keep_until_you_give_it)
