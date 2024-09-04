ee8e9ac6d4a76f1d9138235462872e56f8ab83e5
Tank2007 <ccumm511@evit.edu>
2024-09-04 12:35:57 -0700

    exported from Skill Struck

---
+++ python1/Concatenating Numbers Continued.py
@@ -0,0 +1,8 @@
+'''string1 = "Amout of time in scp:sl {} amount of hours I put in scp:sl {}"
+hours = 2000
+time = 3
+print(string1.format(hours, time))'''
+#Challenge 1
+user_input = input("Number of miles ")
+user_input2 = input("Total number of miles ")
+
