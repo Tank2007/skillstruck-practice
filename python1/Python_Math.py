da9ccf38b530a33e4d4b56b1f6a6ea9c831b1ea3
Tank2007 <ccumm511@evit.edu>
2024-09-04 12:36:00 -0700

    exported from Skill Struck

---
+++ python1/Python_Math.py
@@ -0,0 +1,19 @@
+'''bees = 5
+flowers = 14
+answer = bees * flowers
+print(answer)
+*Multiplication - Subtraction + Addition / Division '''
+
+'''caterpillars = 3
+leaves = 25
+print(caterpillars * leaves)'''
+
+#Challenge 1
+'''apple = int(input("What is your first number? "))
+candy = int(input("What is your second number? "))
+result = candy + apple 
+print(result)'''
+#Challenge 2 
+nummber_of_people = int(input("Enter nummber of people "))
+money_per_person = int(input("Enter the amount each person brings "))
+print( nummber_of_people * money_per_person)
