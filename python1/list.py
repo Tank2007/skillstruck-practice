4811aa75e1f8f7edc2c885a618275c2a3f80b076
Tank2007 <ccumm511@evit.edu>
2024-09-05 14:14:37 -0700

    exported from Skill Struck

---
+++ python1/list.py
@@ -0,0 +1,8 @@
+#Notes 
+'''list a data group that is ordered (numbered) and changeable (you can change the list items).  Allows duplicates
+
+Tuple: a data group that is unordered and unchangeable.  Allows duplicates.
+
+Dictionary: a data group that is unordered, changeable and indexed.  No duplicates.'''
+games = ["scp:sl", "hearts of iron iv", "fallout4", "victor vran"]
+print(games)
