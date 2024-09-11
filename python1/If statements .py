0ae9e44b18fcaddf9d90c1f769f9abe9e49a1e77
Tank2007 <ccumm511@evit.edu>
2024-09-11 13:07:40 -0700

    exported from Skill Struck

---
+++ python1/If statements .py
@@ -0,0 +1,34 @@
+'''Greater than: >
+
+Greater than or equal to: >=
+
+Less than: <
+
+Less than or equal to: <=
+
+Equal to ==
+An important note about the use of the "=" equals sign.  If we are trying to see if conditions are equal, we need to use TWO equals signs.  One equals sign is used when declaring variables.'''
+'''cleab_age = 16
+jon_age = 16 
+if cleab_age == jon_age:
+    print("They are the same age")
+else:
+    print("They are differnt ages")'''
+#Challegne 1
+'''the_user_input = input("pick a number ")
+user_input = input("pick a another number ")
+
+
+if the_user_input < user_input:
+    smallest = the_user_input
+else:
+    smallest = user_input
+print(smallest)'''
+#print(f"The smallest number is: {smallest}")
+#Challenge 2
+
+the_user_input = int(input("pick a number "))
+if the_user_input % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
