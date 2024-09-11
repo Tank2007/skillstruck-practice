fa77f0feb74281edde8083420c4e0da236951dd4
Tank2007 <ccumm511@evit.edu>
2024-09-11 13:07:41 -0700

    exported from Skill Struck

---
+++ python1/Conditionals.py
@@ -0,0 +1,56 @@
+'''AND Condition
+If you want to check if TWO different conditions are both true, you can combine two condition statements by using an AND condition.  
+
+You do this by putting the word and between conditions.
+
+condition1 and condition2'''
+
+# Here is an example:
+'''your_pet = "gerbil"
+your_need = "exercise"
+
+if your_pet == "dog" and your_need == "animal companin":
+    print("You are allowed to bring your pet insied the store.")
+else:
+    print("you cannot bring your pet insied the store.")'''
+
+
+''' AND Condition
+If you want to check if TWO different conditions are both true, you can combine two condition statements by using an AND condition.  
+
+You do this by putting the word and between conditions.
+
+condition1 and condition2'''
+
+#Here is an example:
+'''your_pet = "gerbil"
+
+if your_pet == "dog" or your_pet == "repile":
+    print("You are allowed to bring your pet insied the store.")
+else:
+    print("you cannot bring your pet insied the store.")'''
+
+
+'''age = int(input("Please enter your age: "))
+
+license = input("Do you have your license? (yes/no): ").lower() == "yes"
+
+if age >= 16 and license == True:
+    print("You are old enough to drive.")
+else:
+    print("You are not able to drive.")'''
+
+    
+'''number = int(input("Enter a number 0 to 100 "))
+
+if number < 50 or number == 50:
+    print("Fewer than average")
+else:
+    print("More than average")'''
+
+hight = int(input("Enter your hight in inches "))
+
+if hight < 50 or hight == 49:
+    print("You can't ride the roller coaster.")
+else:
+    print("You can ride the roller coaster.")
