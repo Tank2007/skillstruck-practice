f3f04b344f26da813a0ce232d0455a9b0550495d
Tank2007 <ccumm511@evit.edu>
2024-09-19 12:34:48 -0700

    exported from Skill Struck

---
+++ Python2/Using Integers from a List.py
@@ -0,0 +1,43 @@
+'''games = [2, 4, 5, 6, 8, 10,]
+print(games[1] + 10)'''
+
+#rounding
+#number in the () means how much decimals there will be
+'''pies = 10 
+
+shared = pies/3
+
+print(round(shared, 0))'''
+
+#range
+
+'''for x in range(0, 20, 3):
+    print(x)'''
+
+'''lasers = [3, 10, 4, 15, 11]
+
+print(lasers [2] * 10)
+for x in range(0,10):
+    print(x)
+
+for x in range(0, 10, 2):
+    print(x)'''
+
+'''number_list = [int(n) for n in input().split()]
+
+total_sum = 0
+
+for number in number_list:
+    total_sum += number
+
+print(total_sum)'''
+
+'''my_list = [int(n) for n in input().split()]
+
+
+product = 1
+
+for number in my_list:
+    product *= number  
+
+print(product)'''
