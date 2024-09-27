47f3c1f0060927da727891590ad0b9c486aa846a
Tank2007 <ccumm511@evit.edu>
2024-09-27 14:33:03 -0700

    exported from Skill Struck

---
+++ Python2/Multiple Parameters in Python Functions.py
@@ -0,0 +1,35 @@
+'''def gifts(first, second, third):
+    print("Your first choice for a birthday gift would be " + first)
+    print("Your second choice for a birthday gift would be " + second)
+    print("Your thrid choice for a birthday gift would be " + third)
+
+gifts("bike", "basketball", "bo6")
+gifts("speaker", "tickets", "nothing")'''
+
+'''def gifts(first, second):
+    print("Your first choice for a birthday gift would be " + first)
+    print("Your second choice for a birthday gift would be " + second)
+
+gifts("bike", "basketball")
+gifts("speaker", "bo6")'''
+'''num1 = input("Enter a number")
+num2 = input("Enter a number")
+num3 = input("Enter a number")
+def find_different_number(num1, num2, num3):
+    if num1 != num2 and num1 != num3:
+        print(1) 
+    elif num2 != num1 and num2 != num3:
+        print(2)  
+    else:
+        print(3)  
+
+find_different_number(num1, num2, num3)'''
+
+
+num1 = input("name a fruit ")
+num2 = input("name a fruit ")
+num3 = input("name a fruit ")
+def different_fruits(num1, num2, num3):
+    print(num1 + num2 + num3)
+
+different_fruits(num1, num2, num3)
