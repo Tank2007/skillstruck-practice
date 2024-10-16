803252c0110aca17280f04b5180969f4a86efeb0
Tank2007 <ccumm511@evit.edu>
2024-10-16 13:55:58 -0700

    exported from Skill Struck

--- Python2/Python Dictionaries.py
+++ Python2/Python Dictionaries.py
@@ -1,1 +1,49 @@
-mountains = 
+'''classmates = {
+    "Cleab" : 8,
+    "jon" : 12,
+    "Connor" :10
+}
+
+print(classmates)'''
+
+#the first item is a key the second item in the paring is called a value 
+
+'''mountains = {
+    "Everest" : 8848,
+    "Kilimanjaro" : 5895,
+    "Timpanogos" : 3582,
+    "Vesuvius" : 1281
+}
+
+
+print(mountains)
+print(mountains["Vesuvius"])
+mountains["Kilimanjaro"] = 6000 
+print(mountains)'''
+
+'''games = {
+    "scp:sl" : 1,
+    "r6" : 2,
+    "thefinals" : 3,
+    "fallout4" : 4,
+    "fallout76" : 5,
+    "carrier_command_2" : 6,
+    "stellaris" : 7, 
+    "victorvran" : 8,
+    "don't_starve_together" : 9,
+    "farcry5" : 10
+}
+print(games)'''
+
+
+games1 = {
+     "carrier_command_2" : 1,
+    "stellaris" : 2,
+    "victorvran" : 3,
+    "don't_starve_together" : 4,
+    "farcry5" : 5
+}
+
+games2 = ["scp:sl", "r6", "thefinals", "fallout4", "fallout76"] 
+print(games1)
+print(games2)
