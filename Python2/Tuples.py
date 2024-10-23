52a59b440bd4cc6f1ba63bdab9a0b79e0c03df6e
Tank2007 <ccumm511@evit.edu>
2024-10-23 12:58:45 -0700

    exported from Skill Struck

---
+++ Python2/Tuples.py
@@ -0,0 +1,23 @@
+'''grades = ("95", "70", "85", "92", "100")
+print(grades)
+print(grades[-2])
+range(grades[1:3])'''
+
+'''friends = (3, 5, 7, 8, 10, 2, 12, 4)
+
+userinput = input("Enter a number: ").strip("[]")
+
+userinput = int(userinput)
+
+if userinput in friends:
+    print("Yes")
+else:
+    print("No")'''
+
+
+games = ("SCP:SL", "Fallout4", "R6", "Thefinals", "Victorvran", "fallout76", "falloutnewveags", "windward")
+
+print(games[-3])
+print(games[5])
+for x in range(0, 4):
+    print(games[x]) 
