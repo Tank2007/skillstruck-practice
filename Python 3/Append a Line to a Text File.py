11ddbae0cbd5d95559ee31f644d2e91b1b7119c2
Tank2007 <ccumm511@evit.edu>
2024-10-25 13:40:35 -0700

    exported from Skill Struck

---
+++ Python 3/Append a Line to a Text File.py
@@ -0,0 +1,18 @@
+""" r read a append w write these are some commnads that you can do'''
+''' to use the commands you use file = open ("example.txt", "a") 
+to use the write command you do file.write("This is the new line we added 
+file.close()"""
+
+'''file - open("example.txt", "a")
+file.write("I love programming!")
+print(file.read())'''
+
+
+'''file = open("example.txt", "a")
+file.write("From your pen pal")
+print(file.read())'''
+
+
+file = open("example.txt", "a")
+file.write("Quote was said by Gandhi")
+print(file.read())
