74024a9f0e48b390c085143941fff8a3004a1583
Tank2007 <ccumm511@evit.edu>
2024-10-25 13:40:33 -0700

    exported from Skill Struck

---
+++ Python 3/Reading_Files.py
@@ -0,0 +1,14 @@
+'''file = open("Reading Files.txt", "r")
+print(file.read())'''
+
+
+
+'''file = open("cobra.txt", "r")
+print(file.read())  
+file.close()'''
+
+
+
+'''file = open("speech.txt", "r")
+print(file.read())
+file.close()'''
