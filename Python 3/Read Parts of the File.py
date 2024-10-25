0e8d015780e9e5edc25f4583016b8fc15dd8d065
Tank2007 <ccumm511@evit.edu>
2024-10-25 13:40:35 -0700

    exported from Skill Struck

---
+++ Python 3/Read Parts of the File.py
@@ -0,0 +1,39 @@
+''' this will print out the entire txt
+file = open("exampe.txt", "r")
+print(file.read())
+
+this will print out the first 5 characters 
+
+file = open("exampe.txt", "r")
+print(file.read(5)) also this does use indexing'''
+
+'''print(file.readline()) this will print a line'''
+
+'''file = open("reading_Files.txt", "r")
+print(file.read(10))
+print(file.readline())
+print(file.readline())
+file.close()'''
+
+
+'''userinput = input("Enter some characters")
+
+file = open("userinput.txt", "r")
+print(file.read(userinput))
+file.close()'''
+
+'''userinput = input("enter some lines of code")
+
+file = open(userinput.txt, "r")
+print(len(file.readline(userinput)))
+file.colse()'''
+
+
+
+'''file = open("Reading Files.txt", "r")
+
+
+print(len(file.readlines()))
+
+
+file.close()'''
