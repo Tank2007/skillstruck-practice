bf576e71d1754b695922d04607f6687afba12502
Tank2007 <ccumm511@evit.edu>
2024-10-25 13:40:38 -0700

    exported from Skill Struck

---
+++ Python 3/codewars.py
@@ -0,0 +1,9 @@
+age is < 14 or == 0:
+    print("they drink toddy") 
+elif age < 18 or <= 15:
+    print("they drink coke")
+elif age < 21 or <= 18:
+    print("they drink beer")
+else age == 21 or < 21:
+    print("they drink whisky")
+def drink_by_age(age):
