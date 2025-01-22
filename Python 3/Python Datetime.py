d83d8554cfd65ff8d78443ce6d0137f663ff1093
Tank2007 <ccumm511@evit.edu>
2025-01-22 13:37:47 -0700

    exported from Skill Struck

---
+++ Python 3/Python Datetime.py
@@ -0,0 +1,21 @@
+import datetime 
+x = datetime.datetime.now()
+print(x)
+
+
+import datetime
+today = datetime.date.today()
+print(today)
+
+
+
+
+'''import datetime 
+
+x = datetime.datetime.now()
+
+print(x.strftime("%X"))'''
+
+
+'''%a weekday short = Thurs %A weekday long thursday %b month short nov %B month long novmeber 
+%y 2 digit year 22 %M minute 35 %S second 30'''
