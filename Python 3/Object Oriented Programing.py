9cba3cbbe327f245ececa7d9a5c718c0610564fc
Tank2007 <ccumm511@evit.edu>
2025-01-22 13:37:52 -0700

    exported from Skill Struck

---
+++ Python 3/Object Oriented Programing.py
@@ -0,0 +1,29 @@
+'''friend = {"name": "Jon", "age": 17, "has_dog": True}
+print(friend)
+
+class Person:
+    
+    def __init__(self, name, age, gender):
+     self.name = name 
+     self.age = age
+     self.gender = gender'''
+
+
+'''class dog:
+    def __init__(self, name, color, size):
+        self.name = name 
+        self.color = color
+        self.size = size'''
+
+'''class car:
+    def __init__(self, kind, color, year):
+        self.kind = kind
+        self.color = color
+        self.year = year'''
+
+
+class tree:
+    def __init__(self, fruit, height, leaf, ):
+        self.fruit = fruit
+        self.height = height
+        self.leaf = leaf
