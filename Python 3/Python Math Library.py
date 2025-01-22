221c897ed188578c01744ca34a94ed8b717a8f40
Tank2007 <ccumm511@evit.edu>
2025-01-22 13:37:47 -0700

    exported from Skill Struck

---
+++ Python 3/Python Math Library.py
@@ -0,0 +1,43 @@
+'''# rounding up
+import math 
+x = math.ceil(3.2)
+print(x)
+
+#rounding down
+import math
+x = math.floor(3.2)
+print(x)
+
+
+
+#pi
+import math
+x = math.pi
+print(x)
+
+
+#sq root
+import math
+x = math.sqrt(25)
+print(x)
+
+# Multiply by a Power
+import math 
+x = math.pow(2,4)
+print(x)'''
+
+'''import math
+
+people = int(input("Enter the number of people in the town: "))
+bikes = int(input("Enter the number of bikes in the town: "))
+
+
+people_per_bike = math.ceil(people / bikes)
+
+print(f"In this town, for every bike that exists there are {people_per_bike} people.")'''
+
+side_length = int(input().strip())
+
+volume = pow(side_length, 3)
+
+print(volume)
