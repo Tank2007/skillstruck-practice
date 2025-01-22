4c3d52fd0c6bd04826f653d887c341f8c3367077
Tank2007 <ccumm511@evit.edu>
2025-01-22 13:37:53 -0700

    exported from Skill Struck

---
+++ Python 3/Objects Continued.py
@@ -0,0 +1,9 @@
+class Vacation:
+    def __init__(self, place, distance, weather):
+        self.place = place
+        self.distance = distance
+        self.weather = weather 
+def tuesday(self):
+    print("We will be hiking on Tuesday")
+
+summer = Vacation("Hawaii, 2000, Sunny")
