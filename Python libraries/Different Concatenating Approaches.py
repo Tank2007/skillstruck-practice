d8ad76d80182e53971f865fbf54c07af7035b79a
Tank2007 <tankcam8@gmail.com>
2025-03-26 14:21:24 -0700

    exported from Skill Struck

---
+++ Python libraries/Different Concatenating Approaches.py
@@ -0,0 +1,51 @@
+# worst way of concatenating
+'''name = "Jon"
+age = 16
+grade = 11
+print("Hello my name is " + name + " and I am " + str(age) + " Years old and am in grade " + str(grade))'''
+
+# F string way
+'''name = "Connor"
+age = 17
+grade = 11
+print(f"Hello my name is {name}  and I am  {age}  years old and am in grade  {grade}")'''
+
+#comma 
+'''product = "eggs"
+price = 10
+print("Today we have", product, "for buying", price)'''
+
+
+'''name = "Jon"
+location = "USA"
+dogs = 1
+
+print("My name is " + name + "and I have" + str(dogs) + "dogs and live in " + location)
+
+print(f"My name is {name} and I have {dogs} dogs and live in {location}")
+
+print("My name is", name, "and I have", dogs, "dogs and live in", location)'''
+
+'''football = 27 
+basketball  = 110 
+baseball = 5 
+soccer = 2 
+tennis = 6 
+
+
+print(football, "football Score", baseball, "basketball Score", baseball, "soccer Score", soccer, "Score", tennis)'''
+
+
+'''item1 = "Apples"
+item2 = "Bananas"
+item3 = "Carrots"
+item4 = "Oranges"
+item5 = "Eggs"
+
+quantity1 = 5
+quantity2 = 3
+quantity3 = 4
+quantity4 = 2
+quantity5 = 12
+
+print(f"I need to get {quantity1} {item1}, {quantity2} {item2}, {quantity3} {item3}, {quantity4} {item4} and {quantity5} {item5}")'''
