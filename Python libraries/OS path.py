0c541efe6b6139783b5516abb9e3607f0fcc1984
Tank2007 <tankcam8@gmail.com>
2025-03-26 14:21:26 -0700

    exported from Skill Struck

---
+++ Python libraries/OS path.py
@@ -0,0 +1,24 @@
+'''import os
+x = os.path.isdir("C:\\Users")
+print(x)'''
+
+
+# how to remove a file in os
+'''import os 
+os.path.remove("C:\\Users\owl.png")'''
+
+
+#basename
+'''import os 
+x = 
+os.path.basename("/sqirrel/elephant.png")
+print(x)'''
+
+# gives you the folder where the file is located
+'''import os
+variablename = 
+os.path.dirname("/sqirrel/elephant.png")
+print(variablename)'''
+
+
+
