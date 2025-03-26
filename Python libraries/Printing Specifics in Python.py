5d401b72a3ae6bde24d9e1a25598e70e48bf0496
Tank2007 <tankcam8@gmail.com>
2025-03-26 14:21:24 -0700

    exported from Skill Struck

---
+++ Python libraries/Printing Specifics in Python.py
@@ -0,0 +1,18 @@
+# print on new print("I love \n to code")
+# it will print I love 
+#                to code 
+
+'''print("Cleab and Jon, \n play game's together")'''
+# add a tab print(I love \t to code) will print I love                   to code
+'''print("Connor and jon are \t far from each other")'''
+
+# Carriage Return print("Today is \r a new day") this will print (a new day)
+'''print("Hi \r hallo")'''
+
+
+'''print(" \t \t \t \t Senior")
+print("\t \t \t Junior")
+print("\t \t Sophomore")
+print("\t Freshman")'''
+
+print("All for One  \n \t and One for All!")
