f07dad697dd0a48156e6fbfff801733941ddd016
Tank2007 <tankcam8@gmail.com>
2025-03-26 14:21:19 -0700

    exported from Skill Struck

---
+++ Python 3/Stacks.py
@@ -0,0 +1,32 @@
+'''stack = []
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+stack.append('d')
+stack.pop()
+print(stack)'''
+
+
+'''first = "r" 
+second = "t"
+third = "s" 
+fourth = "y"
+fifth = "o"
+stack = []
+stack.append(third)
+stack.append(second)
+stack.append(fifth)
+stack.append(first)
+stack.append(fourth)'''
+
+
+answer = ["c", "r", "o", "a", "k"]
+
+answer.pop()
+answer.pop()
+answer.append("o")
+answer.append("k")
+
+print(answer)
+
